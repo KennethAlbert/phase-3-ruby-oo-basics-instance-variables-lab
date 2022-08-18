d21062cd066d22941e221d2c8447eff07fb3a9e4
Kenneth Albert <gichuka@gmail.com>
2022-08-18 16:54:19 +0300

    Done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,12 @@
 class Dog
-
-end
+    # setter method
+    def name=(dogs_name)
+        @this_dogs_name = dogs_name
+    end
+  
+    # getter method
+    def name
+     
+      @this_dogs_name 
+    end
+  end
